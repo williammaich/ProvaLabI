b4ffa2152025f2d63bd8ed44720895a1cca09e17
Elvis <elvistavares@outlook.com>
2017-07-10 14:08:24 -0300

    Add files via upload

--- src/prova/Produto.java
+++ src/prova/Produto.java
@@ -11,4 +11,75 @@ package prova;
  */
 public class Produto {
     
+    
+    String marca;
+    String nome;
+    int volume;
+    double valorCompra;
+    
+    double valorVenda;
+    double valorLitro;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public double getValorCompra() {
+        return valorCompra;
+    }
+
+    public void setValorCompra(double valorCompra) {
+        this.valorCompra = valorCompra;
+    }
+
+    public double getValorVenda() {
+        return valorVenda;
+    }
+
+    public void setValorVenda(double valorVenda) {
+        this.valorVenda = valorVenda;
+    }
+
+    public double getValorLitro() {
+        return valorLitro;
+    }
+
+    public void setValorLitro(double valorLitro) {
+        this.valorLitro = valorLitro;
+    }
+    
+    
+    
+    
+    public void AdcionarProdEstoque(){ //comprar
+        
+    }
+    
+    public void PesquisarProd(){ // pesquisar a disponibilidade de produto
+        
+    }
+    
+    public void VenderProd(){ // vender produtos
+        
+    }
 }
